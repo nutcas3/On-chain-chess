3b8bddc05d2727a1a0e18b26822b1598097c9a6a
nutcas3 <mauricee423@gmail.com>
2024-08-20 19:21:21 +0300

    feat: Define game constants for tracking game state, player colors, and piece types. Implement bit masks for efficient color and piece type handling.

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,26 @@ use chess_engine::{Board, BoardBuilder, Color, GameResult, Move, Piece, Position
 /// Import items from the SDK. The prelude contains common traits and macros.
 use stylus_sdk::{alloy_primitives::U256, console, msg, prelude::*};
 
+// game status
+const CONTINUING: u8 = 1;
+const ILLEGAL_MOVE: u8 = 2;
+const STALEMATE: u8 = 3;
+const VICTORY: u8 = 4;
+
+
+// colors
+const WHITE: u8 = 0;
+const BLACK: u8 = 1;
+
+/// Piece types
+const PAWN: u8 = 1;
+const KNIGHT: u8 = 2;
+const BISHOP: u8 = 3;
+const ROOK: u8 = 4;
+const QUEEN: u8 = 5;
+const KING: u8 = 6;
+
+/// Bit masks
+const COLOR_MASK: u8 = 1;
+const PIECE_TYPE_MASK: u8 = 7;
+
