b37645c1b337c990aa99ddc27e7bf5c95388d1f8
nutcas3 <mauricee423@gmail.com>
2024-08-20 20:36:00 +0300

    feat: Implement OnchainChess smart contract for chess game management
    - Added OnchainChess contract with state management for chess games
      - `total_games`: Tracks the total number of chess games started
      - `pending_game`: Stores a single pending game while waiting for a second player
      - `games`: Mapping to store information for each chess game
    
    - Defined `GameInfo` struct to hold chess game details:
      - `player_one`: Address of player one (White)
      - `player_two`: Address of player two (Black)
      - `game_status`: Status of the game (0: PENDING, 1: CONTINUING, 3: STALEMATE, 4: VICTORY)
      - `turn_color`: Current player turn (0: White, 1: Black)
      - `victor`: The victor of the game (0: White, 1: Black)
      - `board_state`: State of the chessboard
    
    - Introduced functionality to manage chess games on-chain, enabling persistence of game state and player turns.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
@@ -6,9 +5,8 @@ extern crate alloc;
 #[global_allocator]
 static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
 
-use alloy_primitives::{Address,U8};
+use alloy_primitives::{Address, U8};
 use chess_engine::{Board, BoardBuilder, Color, GameResult, Move, Piece, Position};
-
 
 /// Import items from the SDK. The prelude contains common traits and macros.
 use stylus_sdk::{alloy_primitives::U256, console, msg, prelude::*};
@@ -18,7 +16,6 @@ const CONTINUING: u8 = 1;
 const ILLEGAL_MOVE: u8 = 2;
 const STALEMATE: u8 = 3;
 const VICTORY: u8 = 4;
-
 
 // colors
 const WHITE: u8 = 0;
@@ -36,3 +33,29 @@ const KING: u8 = 6;
 const COLOR_MASK: u8 = 1;
 const PIECE_TYPE_MASK: u8 = 7;
 
+sol_storage! {
+    #[entrypoint]
+    pub struct OnchainChess{
+      /// Total games of chess started
+      uint256 total_games;
+      /// Used to store a single pending game while waiting for a player two to join.
+      uint256 pending_game;
+      /// Stores info for each chess game
+      mapping(uint256 => GameInfo) games;
+    }
+
+    pub struct GameInfo{
+    /// Player 1 is WHITE
+    address player_one;
+    /// Player 2 is BLACK
+    address player_two;
+    /// PENDING (waiting second player) = 0, CONTINUING = 1, STALEMATE = 3, or VICTORY = 4
+    uint8 game_status;
+    /// Player turn 0 = WHITE; 1 = BLACK
+    uint8 turn_color;
+    /// 0 = WHITE; 1 = BLACK
+    uint8 victor;
+    /// All the info needed to rebuild the board
+    uint256 board_state
+    }
+}
